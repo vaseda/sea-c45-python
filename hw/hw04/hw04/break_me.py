cb7e2aaa628b524ab521eaadb7006c4a127b3482
vaseda <lenayan777.gmail.com>
2015-09-16 13:53:03 -0700

    Correted mistyping in comments

--- hw/hw04/hw04/break_me.py
+++ hw/hw04/hw04/break_me.py
@@ -1,18 +1,23 @@
+
 import sys
 import math
 
 
 def exhibit_name_error():
-    # replace tis line with smth that exibits
+    # replace this line with something that exhibits
     # a name error
     lena()
 
 
 def exhibit_attribute_error():
+    # replace this line with something that exhibits
+    # a attribute error
     math.lena
 
 
 def exhibit_type_error():
+    # replace tis line with something that exhibits
+    # a type error
     a = "Hello"
     b = 123
     c = a + b
