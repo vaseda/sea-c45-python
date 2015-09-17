b67fd6bf8fc2332e5e4b0dedebac139cfa074954
vaseda <lenayan777.gmail.com>
2015-09-16 20:49:30 -0700

    Some code

---
+++ hw/hw07/fibonacci.py
@@ -0,0 +1,6 @@
+def fibonacci(n):
+        """ Returns the nth number from the Febonacci sequence.
+            Example: fibonacci(0) == 1
+            Example: fibonacci(1) == 1
+        """
+return 1       
