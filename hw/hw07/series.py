dd17f9de1402ca594de6f8951a5baad21f0ffeb1
vaseda <lenayan777.gmail.com>
2015-09-18 16:43:28 -0700

    Added test fibonacci function.

--- hw/hw07/series.py
+++ hw/hw07/series.py
@@ -11,7 +11,6 @@ def fibonacci(n):
 
         for i in range(n-1):
             (a, b) = (b, a + b)
-
         return b
 
 
@@ -28,7 +27,26 @@ def lucas(n):
 
         for i in range(n-1):
             (a, b) = (b, a + b)
-
         return b
 
 
+def sum_series(n, seed1=1, seed2=1):
+        """ The required parameter determines which element in the series to print.
+        """
+        a = seed1
+        b = seed2
+
+        if(n == 0):
+            return a
+
+        for i in range(n-1):
+            (a, b) = (b, a + b)
+        return b
+
+
+if __name__ == "__main__":
+    # Test fibonacci
+    assert(fibonacci(0) == 1)
+    assert(fibonacci(1) == 1)
+    assert(fibonacci(2) == 2)
+    assert(fibonacci(3) == 3)
